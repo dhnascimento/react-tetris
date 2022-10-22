609ae2cfcff7bc6c7a60adf444d37d83466623a4
dhnascimento <danielhn@gmail.com>
2022-10-22 13:17:05 -0400

    Change color when block hits last row

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import { useState, useEffect } from 'react';
-import _ from 'lodash';
+import _, { last } from 'lodash';
 import './App.css';
 
 function App() {
 
-  const [count, setCount] = useState(0);
   const [brickPosition, setBrickPosition] = useState({ row: 0, column: 0 });
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (brickPosition.row <= rows[rows.length - 1] + 1) {
-        setBrickPosition({ ...brickPosition, row: brickPosition.row + 1 });
-      }
-    }, 2000);
-  });
+  const [usedPositions, setUsedPositions] = useState([]);
+  const [startingColumn, setStartingColumn] = useState(0);
 
   const rows = _.range(0, 10);
   const columns = _.range(0, 10);
+  const lastRow = rows[rows.length - 1];
+  const lastColumn = columns[columns.length - 1];
+
+  const isPositionUsed = (row, column) => {
+    return !!usedPositions.filter((coord) => _.isEqual(coord, [row, column])).length;
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (brickPosition.row < lastRow) {
+        setBrickPosition({ ...brickPosition, row: brickPosition.row + 1 });
+      }
+      if (brickPosition.row === lastRow) {
+        setUsedPositions([...usedPositions, [brickPosition.row, brickPosition.column]]);
+        setBrickPosition({ ...brickPosition, row: 0, column: startingColumn + 1 });
+        setStartingColumn(startingColumn + 1);
+        if (startingColumn > lastColumn) setStartingColumn(0);
+      }
+    }, 1000);
+  });
 
   return (
     <main id="tetris_frame" style={{
@@ -49,7 +62,7 @@ function App() {
                     return (
                       <div key={`row-${row}-column-${column}`}
                         style={{
-                          backgroundColor: brickPosition.row === row && brickPosition.column === column ? 'blue' : ''
+                          backgroundColor: brickPosition.row === row && brickPosition.column === column ? 'blue' : isPositionUsed(row, column) ? 'yellow' : ''
                         }}
                       >
                         {row}{column}
